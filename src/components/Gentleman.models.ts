4e5bf6f55951f76865aeaf2b3ef3a036aaf9ad76
claramarsango <claraxmarsango@gmail.com>
2023-02-06 19:27:48 +0100

    create gentleman interface

---
+++ src/components/Gentleman.models.ts
@@ -0,0 +1,12 @@
+interface Gentleman {
+  id: number;
+  name: string;
+  status: string;
+  profession: string;
+  twitter: string;
+  picture: string;
+  alternativeText: string;
+  selected: boolean;
+}
+
+export default Gentleman;
